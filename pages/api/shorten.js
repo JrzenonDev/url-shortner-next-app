95b6e9124981f2a4faa9685cdd27acc5dca986a4
JrzenonDev <jrobertoonb@gmail.com>
2022-07-13 00:37:44 -0300

    save data in redis

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -1,8 +1,10 @@
+import { redis } from '../../lib/redis'
+
 export default async function handler(req, res) {
   // get longUrl from body
   const { longUrl } = req.body;
   if (!longUrl || longUrl.length <= 0) {
-    res.status(400).json({ status: 'Error: url not set.' })
+    res.status(400).json({ status: 'Error: url not set.' });
     return;
   }
 
@@ -10,6 +12,8 @@ export default async function handler(req, res) {
   const shortUrl = makeShortUrl(4);
 
   // save to redis
+  let result = await redis.hset('links', { [shortUrl]: longUrl });
+  res.status(200).json({ status: 'Success!' });
 }
 
 const makeShortUrl = (length) => {
