eee7f07373c097f1cb3ef88fa01a0c0e8f166ce4
JrzenonDev <jrobertoonb@gmail.com>
2022-07-20 23:04:23 -0300

    create method getValidUrl

--- middleware.js
+++ middleware.js
@@ -2,11 +2,9 @@ import { NextResponse } from 'next/server';
 import { redis } from './lib/redis'
 
 export default async function middleware(req) {
-  console.log('middleware');
   
   // get the short url
   const pathname = req.nextUrl.pathname;
-  console.log('pathname: ', pathname);
   let parts = pathname.split('/');
   let shortUrl = parts[parts.length - 1];
 
@@ -15,11 +13,23 @@ export default async function middleware(req) {
   const longUrl = await redis.hget('links', shortUrl);
   if (longUrl) {
     // short url found
+    const validUrl = getValidUrl(longUrl);
+
+    return NextResponse.redirect(validUrl);
   } else {
     // short url NOT fount
-    return NextResponse.redirect(req.nextUrl.origin)
+    return NextResponse.redirect(req.nextUrl.origin);
   }
 
   // redurect to the url
 
+}
+
+const getValidUrl = (link) => {
+  if (link.indexOf('http://') == 0 || link.indexOf('https://')  == 0) {
+    // link has https or http -> return as is
+    return link;
+  } else {
+    return `https://${link}`;
+  }
 }
